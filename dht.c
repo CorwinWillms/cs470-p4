b0239c198e760099b67206c2af9422bc25d67dd3
CorwinWillms <willmsca@dukes.jmu.edu>
2022-04-15 18:02:54 -0400

    corwin finished p4

--- dht.c
+++ dht.c
@@ -5,27 +5,27 @@
  *
  * Implementation for distributed hash table (DHT).
  *
- * Name: Corwin Willms, Brooke Sindelar
+ * Names: Corwin Willms, Brooke Sindelar
  *
  */
 
 #include <mpi.h>
 #include <pthread.h>
 #include "local.h"
+#include<stdio.h>   
 
 #include "dht.h"
 
 #define PUT_REQUEST 1
 #define GET_REQUEST 2
 #define BREAK_LOOP 3
+#define GET_SIZE 4
 
-/*
- * Private module variable: current process ID (MPI rank)
- */
+#define CLIENT_TO_SERVER 5
+#define SERVER_TO_CLIENT 6
 
-
+// mutex that guards interaction with local hash table
 pthread_mutex_t hash_guard = PTHREAD_MUTEX_INITIALIZER;
-
 
 // function prototypes
 void* server_loop();
@@ -38,6 +38,7 @@ int hash(const char *name);
 void lock(pthread_mutex_t *mut);
 void unlock(pthread_mutex_t *mut);
 
+// struct for remote requests
 typedef struct remote_request {
     int sender_rank;
     int req_type;
@@ -49,104 +50,122 @@ static int rank;
 pthread_t* server;
 int comm_sz;
 
-
-/**
-HINT: Work incrementally! Don't try to implement the entire protocol at once. 
-Start by augmenting the init and destroy routines to spawn and clean up the 
-server thread (and make sure init returns the process's MPI rank). Then 
-implement the remote put functionality (asynchronously at first), using 
-the end-of-execution dump files to check for correctness. You will also 
-need to make sure that the server threads do not exit until all clients 
-have finished processing commands. Once that is working, implement sync 
-and size (which will require a request/response RPC). This will help you 
-change the put functionality to be synchronous (as required by the above 
-spec). Finally, you can use all of that knowledge to implement the get function. 
-*/
-
-// spawn and manage server thread along w/ dht_destroy()
 /* 
-The server thread should execute a loop that waits for remote procedure 
-call requests from other processes and delegates them to the appropriate 
-local methods
+Initialize MPI then use pthreads to spawn server threads.
+Clients should return with their MPI rank. Server threads
+should enter an infinite loop of receiving from any source.
 */
 int dht_init()
 {
-    // initialize MPI, use pthreads to spawn server threads.
-    // clients should return with their MPI rank, server threads
-    // should enter an infinite loop of receiving from any source
-    MPI_Init(NULL, NULL);
+    int provided;
+    MPI_Init_thread(NULL, NULL, MPI_THREAD_MULTIPLE, &provided);
+    if (provided != MPI_THREAD_MULTIPLE) {
+        printf("ERROR: Cannot initialize MPI in THREAD_MULTIPLE mode.\n");
+        exit(EXIT_FAILURE);
+    }
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
     local_init();
-    //spawn server threads after hashmap initialization
+
     server = malloc(sizeof(pthread_t));
     if (pthread_create(server, NULL, server_loop, NULL) != 0) {
             printf("ERROR: could not create thread\n");
             exit(EXIT_FAILURE);
         }
-    printf("Hello from main thread rank: %d\n", rank);
+
+    // Barrier prevents clients from making requests before all
+    // servers have been created.
+    MPI_Barrier(MPI_COMM_WORLD);
     return rank;
 }
 
+
+/*
+Server function. Server enters an infinite loop answering
+requests from clients. 
+*/
 void* server_loop() {
     bool loop = true;
     long get_answer;
-    struct remote_request *request = malloc(sizeof(struct remote_request));
+    struct remote_request *request = calloc(1, sizeof(struct remote_request));
+    request->req_type = 5;
+
+    // Enter infinite loop until all clients have finished
     while (loop) {
-        printf("Hello from server thread rank: %d\n", rank);
         MPI_Recv(request, // buffer 
-                    sizeof(request), // count
-                    MPI_BYTE, // MPI_datatype (possibly created mpi struct type or some type for C struct)
+                    sizeof(struct remote_request), // count
+                    MPI_BYTE, // MPI_datatype 
                     MPI_ANY_SOURCE, // source
-                    rank, // tag
+                    CLIENT_TO_SERVER, // tag
                     MPI_COMM_WORLD, // MPI_Comm
                     MPI_STATUS_IGNORE); // MPI_status 
+        
         switch (request->req_type) {
             case PUT_REQUEST: 
+                // Add the received key, value pair to local hash table
+                lock(&hash_guard);
                 local_put(request->key, request->value);
+                unlock(&hash_guard);
                 break;
             case GET_REQUEST:
+                // Send requested value to client that requested it
+                lock(&hash_guard);
                 get_answer = local_get(request->key);
-                MPI_Ssend(&get_answer, // buffer (change to either MPI struct or find way to send C struct)
+                unlock(&hash_guard);
+                MPI_Send(&get_answer, // buffer 
                             1, // count
                             MPI_LONG, // MPI_datatype
                             request->sender_rank, // destination
-                            request->sender_rank, // tag
-                            MPI_COMM_WORLD); // MPI_Comm
+                            SERVER_TO_CLIENT, // tag
+                            MPI_COMM_WORLD); // MPI_Comm*/
                 break;
             case BREAK_LOOP:
+                // break out of server loop when all clients are done
                 loop = false;
                 break;
-        }
-        if (true){
-            break;
+            case GET_SIZE:
+                // send local size to the client that asked for it
+                lock(&hash_guard);
+                long my_size = local_size();
+                unlock(&hash_guard);
+                MPI_Send(&my_size, // buffer 
+                            1, // count
+                            MPI_LONG, // MPI_datatype
+                            request->sender_rank, // destination
+                            SERVER_TO_CLIENT, // tag
+                            MPI_COMM_WORLD); // MPI_Comm*/
+                break;
+
         }
     }
     free(request);
     return NULL;
 }
 
-void dht_put(const char *key, long value) // Lam recommends using tag as the receivers rank
+/*
+Adds key, value pair to remote hash table if necessary,
+otherwise it just adds to the local hasj table. 
+*/
+void dht_put(const char *key, long value) 
 {
     // check if key is 'owned' by another process
     int owner = hash(key);
-    if (false)  {// commented until finished with simpler functions(owner != rank) {
+    if (owner != rank)  {
         // remote procedure call to owner
-        struct remote_request *req = malloc(sizeof(struct remote_request));
+        struct remote_request *req = calloc(1, sizeof(struct remote_request));
         req->sender_rank = rank;
         req->req_type = PUT_REQUEST;
-        strncpy(req->key, key, strlen(key));
+        strncpy(req->key, key, sizeof(req->key));
         req->value = value;
-        MPI_Ssend(req, // buffer (change to either MPI struct or find way to send C struct)
-                    sizeof(req), // count
+        MPI_Send(req, // buffer (change to either MPI struct or find way to send C struct)
+                    sizeof(struct remote_request), // count
                     MPI_BYTE, // MPI_datatype
                     owner, // destination
-                    owner, // tag
-                    MPI_COMM_WORLD); // MPI_Comm
+                    CLIENT_TO_SERVER, // tag
+                    MPI_COMM_WORLD); // MPI_Comm*/
         free(req);
     } else {
-        // make sure this doesn't happen during sync (pthreads mutex??)
+        // add key, value pair to hash table
         lock(&hash_guard);
         local_put(key, value);
         unlock(&hash_guard);
@@ -154,100 +173,122 @@ void dht_put(const char *key, long value) // Lam recommends using tag as the rec
 
 }
 
+/*
+Gets key, value pair from remote hash table if necessary,
+otherwise it just gets from the local hasj table. 
+*/
 long dht_get(const char *key)
 {
     long value = KEY_NOT_FOUND;
+
     // check if key is 'owned' by another process
     int owner = hash(key);
-/*
-    if (false) { // commented until finished with simpler functions(owner != rank) {
+    if (owner != rank) { 
         // remote procedure call to owner
-        struct remote_request req;
-        req.sender_rank = rank;
-        req.req_type = PUT_REQUEST;
-        strncpy(req.key, key, strlen(key));
-        MPI_Ssend(&req, // buffer (change to either MPI struct or find way to send C struct)
-                    sizeof(req), // count
+        struct remote_request *req = calloc(1, sizeof(struct remote_request));
+        req->sender_rank = rank;
+        req->req_type = GET_REQUEST;
+        strncpy(req->key, key, sizeof(req->key));
+        MPI_Send(req, // buffer 
+                    sizeof(struct remote_request), // count
                     MPI_BYTE, // MPI_datatype
                     owner, // destination
-                    owner, // tag
-                    MPI_COMM_WORLD); // MPI_Comm
+                    CLIENT_TO_SERVER, // tag
+                    MPI_COMM_WORLD); // MPI_Comm*/
+        free(req);
         
+        // Receive requested value
         MPI_Recv(&value, // buffer 
                     1, // count
-                    MPI_LONG, // MPI_datatype (need to receive a size_t, possibly create struct type)
+                    MPI_LONG, // MPI_datatype 
                     owner, // source
-                    rank, // tag
+                    SERVER_TO_CLIENT, // tag
                     MPI_COMM_WORLD, // MPI_Comm
                     MPI_STATUS_IGNORE); // MPI_status
-    } else {*/
+    } else {
         lock(&hash_guard);
         value = local_get(key);
         unlock(&hash_guard);
-   // }
-
+    }
     return value;
 }
 
-// requires MPI collective Comm
+/*
+Finds the sum of every hash table. 
+*/
 size_t dht_size()
-{ /*
+{ 
     // loop through and MPI_Ssend to all other procs
-    // to let them know to perform the reduction
+    // to let them know they need to send their size
+    struct remote_request *req = calloc(1, sizeof(struct remote_request));
     for (int i = 0; i < comm_sz; i++) {
         if (i != rank) {
-            MPI_Ssend(key, // buffer (change to either MPI struct or find way to send C struct)
-                    strlen(key), // count
-                    MPI_CHAR, // MPI_datatype
-                    owner, // destination
-                    0, // tag
-                    MPI_COMM_WORLD); // MPI_Comm
+            req->sender_rank = rank;
+            req->req_type = GET_SIZE;
+            MPI_Send(req, // buffer 
+                        sizeof(struct remote_request), // count
+                        MPI_BYTE, // MPI_datatype
+                        i, // destination
+                        CLIENT_TO_SERVER, // tag
+                        MPI_COMM_WORLD); // MPI_Comm
         }
     }
+    free(req); 
+    lock(&hash_guard);
+    size_t global_size = local_size();
+    unlock(&hash_guard);
+    long size = 0;
+    // Receive and add up all hash table sizes
+    for (int i = 0; i < comm_sz; i++) {
+        if (i != rank) {
+            MPI_Recv(&size, // buffer 
+                    1, // count
+                    MPI_LONG, // MPI_datatype 
+                    i, // source
+                    SERVER_TO_CLIENT, // tag
+                    MPI_COMM_WORLD, // MPI_Comm
+                    MPI_STATUS_IGNORE); // MPI_status
+        }
+        global_size += size;
+        size = 0; 
+    }
 
-    size_t size = local_size();
-    size_t global_size = 0;
-    MPI_Reduce(&size, // send buffer
-                    &global_size, // receiving buffer 
-                    comm_sz, // count
-                    MPI_LONG, // MPI_datatype
-                    MPI_SUM, //MPI_Op
-                    rank, // 'root'
-                    MPI_COMM_WORLD); // MPI_Comm 
-    return global_size //local_size();
-    */
-    return local_size();
+    return global_size;
 }
 
-// requires MPI collective Comm
+/*
+Syncs all client threads with MPI_BArrier.
+*/
 void dht_sync()
 {
-    // nothing to do in the serial version
-    //MPI_Barrier(MPI_COMM_WORLD);
+    lock(&hash_guard);
+    MPI_Barrier(MPI_COMM_WORLD);
+    unlock(&hash_guard);
 }
 
 void dht_destroy(FILE *output)
 {
-    // Barrier untill all clients have finished processing commands
-    // then MPI_Ssend to all clients telling them to exit infinite loop
-    MPI_Barrier(MPI_COMM_WORLD);
-    struct remote_request req;
-    req.sender_rank = rank;
-    req.req_type = BREAK_LOOP;
-    MPI_Ssend(&req, // buffer (change to either MPI struct or find way to send C struct)
+    // Barrier to make sure all clients are done making requests before
+    // servers are destroyed.
+    MPI_Barrier(MPI_COMM_WORLD);    
+    struct remote_request *req = calloc(1,sizeof(struct remote_request));
+    req->sender_rank = rank;
+    req->req_type = BREAK_LOOP;
+    MPI_Send(req, // buffer 
                     sizeof(req), // count
                     MPI_BYTE, // MPI_datatype
                     rank, // destination
-                    rank, // tag
+                    CLIENT_TO_SERVER, // tag
                     MPI_COMM_WORLD); // MPI_Comm
 
-    local_destroy(output);
-
+    free(req);
     // join server thread
     if (pthread_join(*server, NULL) != 0) {
             printf("ERROR: could not join pthread\n");
             exit(EXIT_FAILURE);
         }
+    free(server);
+    local_destroy(output);
     MPI_Finalize();
 }
 
